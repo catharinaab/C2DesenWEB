5e0c201fb55b8238e219ed3f2c66758ad617f9e9
Catharina Alves Bonella <113545891+catharinaab@users.noreply.github.com>
2024-12-03 10:28:59 -0300

    Add files via upload

---
+++ megaSena.js
@@ -0,0 +1,19 @@
+//Minha possível resolução
+function gerarNumerosMegaSena(quantidade) {
+    const numeros = [];
+    let tentativas = 0;
+
+    while (numeros.length < quantidade) {
+        const aleatorio = Math.floor(Math.random() * 60) + 1;
+        tentativas++;
+        if (!numeros.includes(aleatorio)) {
+            numeros.push(aleatorio);
+        }
+    }
+
+    console.log(`Números gerados: ${numeros}`);
+    console.log(`Total de tentativas: ${tentativas}`);
+    return numeros;
+}
+
+gerarNumerosMegaSena(6);
